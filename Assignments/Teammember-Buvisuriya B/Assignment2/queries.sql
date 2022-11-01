68b2d7b5c3115131abdeb7c50797f52ca1d68c5f
buvisuriya07 <70657998+buvisuriya07@users.noreply.github.com>
2022-11-01 11:42:11 +0530

    Delete queries.sql

--- Assignments/Teammember-Buvisuriya B/Assignment2/queries.sql
+++
@@ -1,9 +0,0 @@
-insert into user values('buvi@gmail.com','buvi',37,'12334');
-insert into user values('aakash@gmail.com','aakash',58,'23345');
-insert into user values('saravana@gmail.com','saravana',55,'13345');
-insert into user values('yoha@gmail.com','yoha',62,'53345');
-
-update user set email='aakash1@gmail.com' where rollnumber=58;
-insert into user values('joseph@gmail.com','josh',59,'22334');
-
-delete from user where username='josh'
